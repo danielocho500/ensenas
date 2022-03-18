269ff25b6d11ad6ba0ff6bcce9df942a2f13ddd7
Daniel Diaz R <danielnochess@gmail.com>
2022-03-18 14:14:27 -0600

    animated

---
+++ src/components/contact/Mapa.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Mapa = () => {
+    return(
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d235.0147788599387!2d-96.92683039527682!3d19.53145917166894!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85db2ffe25c20ccf%3A0x6afa898096b587f9!2zU2XDsWFsw6l0aWNhIEVuc2XDsWFz!5e0!3m2!1ses-419!2smx!4v1647629214909!5m2!1ses-419!2smx" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    )
+}
